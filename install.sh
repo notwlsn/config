86363b246c1ddcd226c32dc95711f32a04db98db
notwlsn <gwillson.camp@gmail.com>
2020-09-08 15:48:41 -0400

    Creating restore script and editing install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# WC 2020
 
 #echo $SHELL
 
---
+++ restore.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#WC 2020
+
+#echo $SHELL
+
+#tmux
+echo "Restoring old tmux.conf..."
+cp tmux_conf_old ~/.tmux.conf
+
+#bash
+echo "Restoring old bashrc..."
+cp bashrc_old ~/.bashrc
+
+#vim 
+echo "Restoring old vimrc..."
+cp vimrc_old ~/.vimrc
+
+echo "Done. Use install.sh to re-install configs."
