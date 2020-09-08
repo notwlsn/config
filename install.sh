21ef2dc83ac7e06c30ebd9da6d3aaba6d03632e2
root <root@localhost.localdomain>
2020-09-08 11:02:12 -0700

    fixing backup loop

--- install.sh
+++ install.sh
@@ -2,25 +2,15 @@
 
 #echo $SHELL
 
-#vim
-while ! [ -f ~/.vimrc ];
-do 
-    echo "no current vimrc, copying..."
-    cp vimrc ~/.vimrc
-done
-echo "vimrc exists, backing it up to vimrc_old..."
-cp ~/.vimrc vimrc_old
-cp vimrc ~/.vimrc
-
 #tmux
-while ! [ -f ~/.tmux.conf ];
+while [ -f ~/.tmux.conf ];
 do 
-    echo "no current tmux.conf in home dir, copying..."
-    echo "please note this only checks home, if your tmux.conf is somewhere else please install manually"
+    echo "tmux.conf exists, backing up to tmux_conf_old"
+    cp ~/.tmux.conf tmux_conf_old 
     cp tmux.conf ~/.tmux.conf
 done
-echo "tmux.conf exists, backing up to tmux_conf_old"
-cp ~/.tmux.conf tmux_conf_old 
+echo "no current tmux.conf in home dir, copying..."
+echo "please note this only checks home, if your tmux.conf is somewhere else please install manually"
 cp tmux.conf ~/.tmux.conf
 
 #bash
@@ -30,5 +20,18 @@ cp ~/.bashrc bashrc_old
 echo "Copying new bashrc..."
 cp bashrc ~/.bashrc
 source ~/.bashrc
+
+#vim
+while [ -f ~/.vimrc ];
+do 
+    echo "vimrc exists, backing it up to vimrc_old..."
+    cp ~/.vimrc vimrc_old
+    cp vimrc ~/.vimrc
+done
+echo "no current vimrc, copying..."
+cp vimrc ~/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ':PluginInstall'
 
 echo "Done. To restore overwrite current configs with thier *_old counterparts in applicable locations"
